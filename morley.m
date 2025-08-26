fb9b40b2094c188ee8acd2fc90cfac0e30307a5c
Devin Greene <devin@greene.cz>
2025-08-26 16:10:03 -0400

    Added some comments

--- morley.m
+++ morley.m
@@ -10,16 +10,19 @@
 F = frac (ZZ[a,b])
 R = F[sr3,r_0..r_1,c_0..c_1,s_0..s_1]
 
+-* The vertices of the triangle *-
 v0 = matrix{{0},{0}}
 v1 = matrix{{1},{0}}
 v2 = matrix{{a},{b}}
 
+-* Vector edges *-
 ar0 = v1 - v0
 ar1 = v2 - v1
 ar2 = v0 - v2
 
 Ones = matrix{{1,1}}
 
+-* Rotation matrix *-
 third = (x,y) -> matrix{{x,-y},{y,x}}
 
 third0 = third(c_0,s_0)
@@ -30,13 +33,15 @@ third1inv = transpose third1
 rot60 = matrix{{1/2,-sr3/2},{sr3/2,1/2}}
 
 I = ideal(
-    sr3^2 - 3,
+    sr3^2 - 3, -- sqrt(3)
     c_0^2 + s_0^2 - 1,
     c_1^2 + s_1^2 - 1,
+    -* Rotate edges into each other *-
     r_0*third0^3*ar0 + ar2,
     r_1*third1^3*ar1 + ar0
    ) 
 
+-* Sum of trisectors is 60 degress *-
 third2 = rot60*third0inv*third1inv
 
 Q = R/I
@@ -45,6 +50,7 @@ third0 = promote(third0,Q)
 third1 = promote(third1,Q)
 third2 = promote(third2,Q)
 
+-* Emulate multiplication by third root of unity *-
 rot120 = matrix{{-1/2,-sr3/2},{sr3/2,-1/2}}
 
 a0 = (v0 | (v0 + third0*ar0))||Ones
@@ -73,6 +79,7 @@ p0_0 = det(b0^{1,2})||det(b0^{2,0})
 p1_0 = det(b1^{1,2})||det(b1^{2,0})
 p2_0 = det(b2^{1,2})||det(b2^{2,0})
 
+-* These are the points of the triangle we wish to show is equilateral *-
 p0 = p0_0*det(b1^{0,1})*det(b2^{0,1})
 p1 = p1_0*det(b2^{0,1})*det(b0^{0,1})
 p2 = p2_0*det(b0^{0,1})*det(b1^{0,1})
