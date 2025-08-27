26aa450bb2d44b256957b12d59c8725ec4beaea7
Devin Greene <devin@greene.cz>
2025-08-27 08:30:32 -0400

    Added (short) midpoint theorem

---
+++ midpoints.m
@@ -0,0 +1,7 @@
+-*
+This is trivialy from the fact that every triangle is an affine transformation
+of the equilateral triangle, and the fact that midpoints are inveriant under
+such transformations.
+*-
+
+print 1
