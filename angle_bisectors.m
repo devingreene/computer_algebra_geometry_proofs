7396251b78f7dfc4116d93d243080dad0bef7cc9
Devin Greene <devin@greene.cz>
2025-12-23 18:49:13 -0500

    Angle bisectors

---
+++ angle_bisectors.m
@@ -0,0 +1,8 @@
+-*
+Start with a circle.  Draw three tangent lines.  These lines form the
+sides of a triangle.  By a straightforward argument it can be shown that
+any triangle can be constructed in this manner.  Clearly the center of
+the circle is the common intersection of the bisectors.
+*-
+
+print true
